374beed55e2a7b1da169f4c4279daf87bd95e214
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2019-04-18 08:43:20 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ep-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'poc';
+}
