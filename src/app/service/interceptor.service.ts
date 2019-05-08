645f2c34063dd433f05460b51a24664d4b60ca43
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2019-05-08 13:23:27 +0530

    add interceptor

--- src/app/service/interceptor.service.ts
+++ src/app/service/interceptor.service.ts
@@ -37,7 +37,7 @@ export class InterceptorService implements HttpInterceptor {
         },((error:any) => {
 
           // this.alertService.success(error.message, true);
-
+ 
         }));
       }
   }
